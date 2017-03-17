802d9d63d677e8c7ea67b5f0bed5a9fe16b3d9f2
Anton A. Lysenkov <al@nerdpeople.com>
2017-03-17 11:07:02 +0400

    Builded files

---
+++ src/ResourceAction.d.ts
@@ -0,0 +1,8 @@
+import { URLSearchParams } from '@angular/http';
+import { Type } from '@angular/core/src/type';
+import { ResourceActionBase } from './Interfaces';
+import { Resource } from './Resource';
+import { ResourceModel } from './ResourceModel';
+export declare function ResourceAction(methodOptions?: ResourceActionBase): (target: Resource, propertyKey: string) => void;
+export declare function appendSearchParams(search: URLSearchParams, key: string, value: any): void;
+export declare function mapToModel(resp: any, model: Type<ResourceModel<Resource>>): any;

03dcae1ee4cc706e84c87b5fa7a2666190d89cb0
Anton Lyisenkov <anton@lyisenkov.ru>
2017-06-08 16:55:41 +0400

    Fix build

--- src/fields/index.d.ts
+++
@@ -1,1 +0,0 @@
-export { ForeignKey } from './ForeignKey';
--- src/fields/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ForeignKey_1 = require("./ForeignKey");
-exports.ForeignKey = ForeignKey_1.ForeignKey;
